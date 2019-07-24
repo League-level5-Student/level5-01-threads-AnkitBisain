0100d7fec56eaec0354848682a8d715bd9304b2f
League Student <league@iMac-14.attlocal.net>
2019-07-24 16:30:02 -0700

    ThreadPool IP

--- src/_04_Thread_Pool/ThreadPool.java
+++ src/_04_Thread_Pool/ThreadPool.java
@@ -1,19 +1,40 @@
 package _04_Thread_Pool;
 
+import java.util.concurrent.ConcurrentLinkedQueue;
+
 public class ThreadPool {
 
-	public ThreadPool(int i) {
+	Thread[] threads;
+	ConcurrentLinkedQueue<Task> taskQueue;
+	
+	public ThreadPool(int totalThreads) {
 		// TODO Auto-generated constructor stub
+		threads = new Thread[totalThreads];
+		for (int i = 0; i < threads.length; i++) {
+			
+		}
+		taskQueue = new ConcurrentLinkedQueue<Task>();
 	}
 
 	public void addTask(Task t) {
 		// TODO Auto-generated method stub
+		taskQueue.add(t);
 		
 	}
 
 	public void start() {
 		// TODO Auto-generated method stub
-		
+		for (int i = 0; i < threads.length; i++) {
+			threads[i].start();
+		}
+		for (int i = 0; i < threads.length; i++) {
+			try {
+				threads[i].join();
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
 	}
 
 		// TODO Auto-generated method stub
